b694a5809442fe90a9a17c84b131c2f38849cf16
steward-fu <g9313716@yuntech.edu.tw>
2019-04-22 18:41:33 +0800

    init

---
+++ include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 1874 */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1874 Sun Apr 7 14:02:21 CST 2019"
+#define LINUX_COMPILE_BY "steward"
+#define LINUX_COMPILE_HOST "debian"
+#define LINUX_COMPILER "gcc version 6.3.0 20170516 (Debian 6.3.0-18)"
